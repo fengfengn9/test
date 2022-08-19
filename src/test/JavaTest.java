2d156378d2797cd8e2544568372b2b3009ecfabf
fengfeng <fengfeng@123.com>
2022-08-19 15:16:08 +0800

    Second fire

--- src/test/JavaTest.java
+++ src/test/JavaTest.java
@@ -8,5 +8,6 @@ public class JavaTest {
     public static void main(String[] args) {
         System.out.println("HI");
         System.out.println("HI2");
+        System.out.println("?????");
     }
 }
