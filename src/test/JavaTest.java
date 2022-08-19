c4c0c4b60e2acdc3085bdf762f221ec396fdf9c6
fengfeng <fengfeng@123.com>
2022-08-19 15:05:21 +0800

    First

---
+++ src/test/JavaTest.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * @author fengfeng
+ * @date 2022/8/19 - ${}
+ */
+public class JavaTest {
+    public static void main(String[] args) {
+        System.out.println("HI");
+    }
+}
